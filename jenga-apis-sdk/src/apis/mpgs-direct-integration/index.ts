a12af2f82a38be69b7eb1ac2dc508ff4bcb44ecf
chariss.chomba <chariss.chomba@equitybank.co.ke>
2023-03-23 16:56:21 +0300

    Add documentation

--- jenga-apis-sdk/src/apis/mpgs-direct-integration/index.ts
+++ jenga-apis-sdk/src/apis/mpgs-direct-integration/index.ts
@@ -2,9 +2,63 @@ import { Base } from "src/base";
 import { generateSignature } from "src/utils/signature";
 
 export class MpgsDirectIntegrationService extends Base {
+    /**
+     * MPGS Validate Payment
+     * 
+     * https://developer.jengaapi.io/reference/mpgs-validate-payment
+     * 
+     * This API will validate payment before card authorization. The API will do the following
+
+    * Check if the merchant is subscribed to the Card APIs service
+
+    * Check if they are subscribed to the MID they are using.
+
+    * Fetch the relevant charges
+
+    * cardNumber and cardSecurity are encrypted using AES encryption then the cypher is base64 encoded.
+
+    * @param options 
+    */
     mpgsValidatePayment(options: {}): Promise<any> { };
+    /**
+     * MPGS Authenticate Payment
+     * 
+     * https://developer.jengaapi.io/reference/mpgs-authenticate-payment
+     * 
+     * This API will be used to authenticate payments on MPGS.cardNumber and cardSecurity are encrypted using AES encryption then the cypher is base64 encoded.
+     * 
+     * @param options
+    */
     mpgsAuthenticatePayment(options: {}): Promise<any> { };
+    /**
+     * MPGS Authorize Payment
+     * 
+     * https://developer.jengaapi.io/reference/mpgs-authorize-payment
+     * 
+     * This API will be initiated by the merchant and will authorize payment on Mastercard.cardNumber and cardSecurity are encrypted using AES encryption then the cypher is base64 encoded.
+     * 
+     * @param options
+    */
     mpgsAuthorizePayment(options: {}): Promise<any> { };
+    /**
+     * MPGS Query Payment
+     * 
+     * https://developer.jengaapi.io/reference/payment-payment
+     * 
+     * This API will query the status of a transaction.
+     * 
+     * @param options
+    */
     mpgsQueryPayment(options: {}): Promise<any> { };
+    /**
+     * MPGS Refund Payment
+     * 
+     * https://developer.jengaapi.io/reference/mpgs-refund-payment
+     * 
+     * This API will refund a payment.
+     * 
+     * @param options
+     * 
+    */
     mpgsRefundPayment(options: {}): Promise<any> { };
 }
--- jenga-apis-sdk/src/apis/send-money-imt/index.ts
+++ jenga-apis-sdk/src/apis/send-money-imt/index.ts
@@ -2,8 +2,47 @@ import { Base } from "src/base";
 import { generateSignature } from "src/utils/signature";
 
 export class SendMoneyImtService extends Base {
+    /**
+     * IMT Within Equity Bank
+     * 
+     * https://developer.jengaapi.io/reference/imt-within-equity-bank
+     * 
+     * @param options
+     * 
+     * Move Funds Within Equity Bank Across Kenya, Uganda, Tanzania, Rwanda & South Sudan.
+    */
     sendMoneyWithinEquityImt(options: {}): Promise<any> { }
+    /**
+     * IMT to Mobile Wallets
+     * 
+     * https://developer.jengaapi.io/reference/imt-to-mobile-wallets
+     * 
+     * This enables your application to send money to telco  wallets across Kenya, Uganda, Tanzania & Rwanda.
+
+     *   Kindly note in order to get a response you will need to test this in production.
+     * 
+     * @param options
+    */
     sendMoneyToMobileWalletImt(options: {}): Promise<any> { }
+    /**
+     * IMT Pesalink - To Bank Account
+     * 
+     * https://developer.jengaapi.io/reference/imt-pesalink-to-bank-account
+     * 
+     * This web service enables an application to send money to a PesaLink participating bank. It is restricted to Kenya.
+     * 
+     * @param options
+    */
     sendMoneyPesalinkToBankAccountImt(options: {}): Promise<any> { }
+    /**
+     * IMT Pesalink - To Bank Mobile
+     * 
+     * https://developer.jengaapi.io/reference/imt-pesalink-to-bank-mobile
+     * 
+     * This web service enables an application to send money to mobile.
+     * 
+     * @param options
+     * 
+    */
     sendMoneyPesalinkToMobileImt(options: {}): Promise<any> { }
 }
